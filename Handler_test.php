2b466313538ebecd823f63f0f9712cd488acd7ad
Pierre Lavency <pierrelavency@hotmail.com>
2016-10-13 00:27:00 +0200

    hnadler unit test

--- Handler_test.php
+++ Handler_test.php
@@ -1,23 +1,42 @@
 
 <?php
+	require_once("UnitTest.php");
+	$logName = basename(__FILE__, ".php");
+	$z=new unitTest($logName);
 
-require_once("Handler.php"); 
+	require_once("Handler.php"); 
+	$show = false;
 
+	$test = [
+		"ERC" => [0=>["lastId"=>3],1=>["CODE"=> "001", "SEVERITY"=> 1], 2 => ["CODE"=> "002", "SEVERITY"=> 2] ], 
+		];
+		
+	$x = Handler::getInstance();
+									$line = "x = Handler::getInstance();";
+									$z->logLine($line);
+	$y = $x-> getFileBase();
+									$line = "y = x-> getFileBase();";
+									$z->logLine($line);
+	$y->objects = $test; 
 
-$test = [
-	"ERC" => [0=>["lastId"=>3],1=>["CODE"=> "001", "SEVERITY"=> 1], 2 => ["CODE"=> "002", "SEVERITY"=> 2] ], 
-	];
+	if ($show){
+		var_dump($y);
+		echo "<br/>";
+	}
+
+	$s = getHandler('file_persist');
+									$line = "s = getHandler('file_persist');";
+									$z->logLine($line);
 	
-$x = Handler::getInstance();
-
-$y = $x-> getFileBase();
-$y->objects = $test; 
-
-var_dump($y);
-echo "<br/>";
+	$r = ($y === $s);
+									$line = "	$r = (y === s);";
+									$z->logLine($line);
 
 
-$y = getHandler("file_persist");
-
-var_dump($y);
+	if ($show){
+		var_dump($r);
+	}
+	
+	
+	$z->save();
 ?>
--- TestSuite_test.php
+++ TestSuite_test.php
@@ -5,5 +5,5 @@ require_once("Logger_test.php");
 require_once("Model_test.php"); 
 require_once("GenHTML_test.php"); 
 require_once("PersistFile_test.php"); 
-
+require_once("Handler_test.php"); 
 ?>
