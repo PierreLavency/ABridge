e8d5918374d9bdcade84fe6d7025dddd251a67fa
Pierre Lavency <pierrelavency@hotmail.com>
2016-11-16 11:51:27 +0100

    Coding standard ok but license tag

--- Logger.php
+++ Logger.php
@@ -1,88 +1,193 @@
 
 <?php
+/**
+ * Logger  class file
+ *
+ * PHP Version 5.6
+ *
+ * @category PHP
+ * @package  ABridge
+ * @author   Pierre Lavency <pierrelavency@hotmail.com>
+ * @link     No
+ */
+ 
+ /**
+ * Logger class
+ *
+ * Loggers are used in many classes to log errors or trace executions
+ * 
+ * @category PHP
+ * @package  ABridge
+ * @author   Pierre Lavency <pierrelavency@hotmail.com>
+ * @link     No
+ */
+class Logger
+{
+    public $lines = [];
+    public $filePath ='C:\Users\pierr\ABridge\Logstore\\';
+    public $fileName;
+    public $name;
 
-class Logger{
+    /**
+     * Constructor
+     *
+     * @param string $name file name to be used if saved. 
+     */   
+    function __construct($name= "defaultLoggerFileName" ) 
+    {
+        $this->name = $name;
+        $this->fileName = $this->filePath.$name.".txt";
+    }
+    
+    /**
+     * Log a line 
+     *
+     * @param string $line to be logged. 
+     *
+     * @return int line number
+     */   
+    public function logLine($line)
+    {
+        $r = count($this->lines);
+        $this->lines[] = $line;
+        return $r;
+    }
 
-	public $lines = [];
-	public $filePath ='C:\Users\pierr\ABridge\Logstore\\';
-	public $fileName;
-	public $name;
-	
-	function  __construct($name= "defaultLoggerFileName" ) {
-		$this->name = $name;
-		$this->fileName = $this->filePath.$name.".txt";
-	}
-	public function logLine ($line){
-		$r = count($this->lines);
-		$this->lines[] = $line;
-		return $r;
-	}
-	
-	public function show ($show=true) {
-		$c = count($this->lines);
-		if (!$c) {
-			$result = ""; 
-			if ($show) {echo $result;}; 
-			return $result;
-			}
-		$result="<br>";
-		for ($i=0;$i<$c;$i++) {
-			$result=$result. "LINE:".$i."<br>";
-			$result=$result. $this->lines[$i];
-			$result=$result."<br>";
-			}
-		$result=$result."<br>";
-		if ($show) {echo $result;}
-		return $result;
-	}
+    /**
+     * Show logged lines 
+     *
+     * @param boolean $show echo or not the output. 
+     *
+     * @return string output string
+     */    
+    public function show($show=true) 
+    {
+        $c = count($this->lines);
+        if (!$c) {
+            $result = ""; 
+            if ($show) {
+                echo $result;
+            }; 
+            return $result;
+        }
+        $result="<br>";
+        for ($i=0;$i<$c;$i++) {
+            $result=$result. "LINE:".$i."<br>";
+            $result=$result. $this->lines[$i];
+            $result=$result."<br>";
+        }
+        $result=$result."<br>";
+        if ($show) {
+            echo $result;
+        }
+        return $result;
+    }
 
-	public function showLine($i){
-		if ($i < count($this->lines)) {
-			echo "LINE:".$j."<br>";
-			echo $this->lines[$i];
-			echo "<br>";
-			return $this->lines[$i];
-		}
-		return 0;
-	}
+    /**
+     * Show a logged line 
+     *
+     * @param int $i line to be shown. 
+     *
+     * @return string output string
+     */     
+    public function showLine($i)
+    {
+        if ($i < count($this->lines)) {
+            echo "LINE:".$j."<br>";
+            echo $this->lines[$i];
+            echo "<br>";
+            return $this->lines[$i];
+        }
+        return 0;
+    }
+ 
+    /**
+     * Get a logged line 
+     *
+     * @param int $i the line number. 
+     *
+     * @return string the line
+     */     
+    public function getLine($i) 
+    {
+        if ($i < count($this->lines)) {
+            return $this->lines[$i];
+        }
+        return 0;
+    }
 
-	public function getLine($i) {
-		if ($i < count($this->lines)) {
-			return $this->lines[$i];
-		}
-		return 0;
-	}
+    /**
+     * Get number of lines logged  
+     *
+     * @return int the number of lines
+     */     
+    public function logSize() 
+    {
+        return count($this->lines);
+    }
+ 
+    /**
+     * Compare with another Logger  
+     *
+     * @param Logger $log the logger to compare with
+     *
+     * @return int 0 if bot are the same
+     */  
+    public function diff($log)
+    {
+        $c = count($this->lines);
+        if (! ($c == $log->logSize())) {
+            return -1;
+        }
+        for ($i=0;$i<$c;$i++) {
+            if (! ($this->lines[$i]==$log->getLine($i))) {
+                $j=$i+1;return $j;
+            }
+        }
+        return 0;
+    }
 
-	public function logSize() {
-		return count($this->lines);
-	}
-	public function diff ($log){
-		$c = count($this->lines);
-		if (! ($c == $log->logSize())) {return -1;}
-		for ($i=0;$i<$c;$i++) {
-			if (! ($this->lines[$i]==$log->getLine($i))){$j=$i+1;return $j;}
-		}
-		return 0;
-	}
-	public function includeLog ($log){
-		$c = $log->logSize();
-		for ($i=0;$i<$c;$i++) {
-			if (! $this->logLine($log->getLine($i))) {return 0;} ; 
-		}
-		$r = $c +1;
-		return $r;
-	}
-	public function save() {
-		$file = serialize($this->lines);
-        $r=file_put_contents($this->fileName,$file,FILE_USE_INCLUDE_PATH);
-		return $r;
-	}
-	public function load() {
+    /**
+     * Include another Logger content  
+     *
+     * @param Logger $log the logger to include
+     *
+     * @return int the number of lines included
+     */  
+    public function includeLog($log)
+    {
+        $c = $log->logSize();
+        for ($i=0;$i<$c;$i++) {
+            if (! $this->logLine($log->getLine($i))) {
+                return 0;
+            } ; 
+        }
+        $r = $c +1;
+        return $r;
+    }
+
+    /**
+     * Save content on file  
+     *
+     * @return int the number of byte saved
+     */  
+    public function save() 
+    {
+        $file = serialize($this->lines);
+        $r=file_put_contents($this->fileName, $file, FILE_USE_INCLUDE_PATH);
+        return $r;
+    }
+     /**
+     * Load content from file  
+     *
+     * @return int the number of byte saved
+     */  
+    public function load() 
+    {
         $file = file_get_contents($this->fileName, FILE_USE_INCLUDE_PATH);
-		$this->lines = unserialize($file);
-		return true;
-	}
-	
+        $this->lines = unserialize($file);
+        return true;
+    }
 
 }
 ?>
